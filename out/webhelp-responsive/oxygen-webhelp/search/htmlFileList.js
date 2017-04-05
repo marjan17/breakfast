c1b86dc4aa0edc09a41cf9601885a34f5756188f
marjan17 <marjan17@vt.edu>
2017-04-04 22:55:28 -0400

    t-recipeone
    spelling

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -1,8 +1,10 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "c-breakfast.html";
-fl["1"]= "t-recipefour.html";
-fl["2"]= "t-recipeone.html";
-fl["3"]= "t-recipethree.html";
-fl["4"]= "t-recipetwo.html";
+fl["1"]= "t-chai.html";
+fl["2"]= "t-recipefour.html";
+fl["3"]= "t-recipeone.html";
+fl["4"]= "t-recipethree.html";
+fl["5"]= "t-recipetwo.html";
+fl["6"]= "t-references.html";
 var doStem = false;searchLoaded = true;
